b27630eda5ca2300c1734f6728e169fd4b98cda2
bonchin-226 <63534064+bonchin-226@users.noreply.github.com>
2022-01-03 15:27:22 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Reactハンズオン！</h2>
+      <p>Reactは難しくないよー</p>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
